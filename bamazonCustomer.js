7547ffcb7124dfe611ae663b0e7f5aeba58e239c
graygabrielle <gray.atkinson.adams@gmail.com>
2019-04-02 11:34:32 -0700

    added more inquirers

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,7 +10,7 @@ function drawTable(){
         console.log("     HERE'S WHAT BAMAZON HAS IN STOCK RIGHT NOW");
         console.log("     ==========================================" + "\n");
         for(let i = 0; i < result.length; i++){
-            console.log("     ID: " + result[i].id + "     PRODUCT: " + result[i].product + "     PRICE: " + result[i].price);
+            console.log("     ID: " + result[i].id + "     PRODUCT: " + result[i].product + "     PRICE: $" + result[i].price);
             productNames.push(result[i].product);
         }
         console.log("\n\n");
@@ -40,13 +40,88 @@ function startPrompt(choices){
     inquirer
     .prompt(questions)
     .then(answers => {
+        let price;
         const item = answers.whatToBuy;
-        console.log("this is what you selected: " + item);
-        const quantity = answers.numOfItems;
-        console.log("this is how many you wanted: " + quantity);
+        database.query("SELECT price FROM products WHERE ?", 
+        {
+            product: item
+        }, 
+        function(err, result){
+            if (err) throw err;
+            price = result[0].price;
+            const quantity = answers.numOfItems;
+            const total = quantity*price;
+            if(quantity==1){
+                console.log(`One ${item} costs $${price}`);
+            }
+            else if(quantity==0){
+                console.log('Please enter a valid number.');
+            }
+            else{
+                console.log(`${quantity} ${item}s will cost $${total}`);
+            }
+            areYouSure(quantity, item);
+        })
+        
 
-                
+
     })
 };
+
+function areYouSure(number, product){
+    const questions = 
+    [{
+        type: 'list',
+        name: 'whatToDo',
+        message: 'What would you like to do?',
+        choices: ['Confirm purchase', 'Change purchase', 'Quit']
+    }];
+
+    inquirer
+    .prompt(questions)
+    .then(answer => {
+        switch (answer.whatToDo){
+            case 'Confirm purchase':
+            if(number>1){
+                product = product + "s";
+            }
+            console.log(`Congratulations on successfully purchasing ${number} ${product}!`);
+            whatNext();
+            break;
+
+            case 'Change purchase':
+            drawTable();
+            break;
+
+            case 'Quit':
+            process.exit();
+            break;
+        }
+    })
+}
+
+function whatNext(){
+    const questions = 
+    [{
+        type: 'list',
+        name: 'whatNext',
+        message: 'What would you like to do next?',
+        choices: ['Make another purchase', 'Quit']
+    }];
+
+    inquirer
+    .prompt(questions)
+    .then(answer => {
+        switch (answer.whatNext){
+            case 'Make another purchase':
+            drawTable();
+            break;
+
+            case 'Quit':
+            process.exit();
+            break;
+        }
+    })
+}
 
 drawTable();
